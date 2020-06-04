e69af2f47bc416edfe929afd99100e883b09a1e2
Jigish Gohil <cyberorg@cyberorg.info>
2020-06-04 15:58:19 +0530

    no colors

--- install.sh
+++ install.sh
@@ -1,16 +1,11 @@
 #!/bin/bash
 
-# Colors to use for output
-RED='\033[0;31m'
-GREEN='\033[0;32m'
-
-
 if [[ $EUID -ne 0 ]]; then
-   echo -e "${RED}This script must be run as root"
+   echo -e "This script must be run as root"
    exit
 fi
 mkdir -p /usr/share/cockpit/assist
 for i in index.html manifest.json network.css.gz assist.js; do
 	wget -c https://raw.githubusercontent.com/cyberorg/assist-cockpit/master/assist/$i -O /usr/share/cockpit/assist/$i
 done
-echo -e "${GREEN} Cockpit Assist plugin installed"
+echo -e "Cockpit Assist plugin installed"

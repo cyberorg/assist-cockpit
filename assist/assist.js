eb52998b2bbde9d21ef9cb3fed62ea5965161898
Jigish Gohil <cyberorg@cyberorg.info>
2020-06-18 14:02:49 +0530

    add socat screen for console

--- assist/assist.js
+++ assist/assist.js
@@ -4,14 +4,30 @@ const output = document.getElementById("output");
 const result = document.getElementById("result");
 const asapply = document.getElementById("asapply");
 const asdiscon = document.getElementById("asdiscon");
+const aconsole = document.getElementById("aconsole");
 
-function asdiscon_run() {
+function asdiscon_run_gui() {
     cockpit.spawn(["pkill", "-USR1", "x11vnc"], {
         superuser: true
     });
 }
 
-function assist_run() {
+function asdiscon_run_console() {
+    cockpit.spawn(["pkill","-USR1", "screen"], {
+        superuser: true
+    });
+}
+
+function asdiscon_run() {
+    if (aconsole.checked == true) {
+        asdiscon_run_console();
+    } else {
+        asdiscon_run_gui();
+    }
+    document.getElementById("asdiscon").disabled = true;
+}
+
+function assist_run_gui() {
     cockpit.spawn(["nohup", "x11vnc", "-auth", "/var/run/lightdm/root/:0", "-q", "-nopw", "-connect_or_exit", AS_SERVER.value + ":" + AS_PORT.value], {
             superuser: true
         })
@@ -19,8 +35,30 @@ function assist_run() {
         .then(assist_success)
         .catch(assist_fail);
     document.getElementById("asapply").value = "Connected";
+    document.getElementById("asdiscon").disabled = false;
     result.innerHTML = "";
     output.innerHTML = "";
+}
+
+function assist_run_console() {
+    cockpit.spawn(["/usr/share/cockpit/assist/socatcmd", "tcp:" + AS_SERVER.value + ":" + AS_PORT.value], {
+            superuser: true
+        })
+        .stream(assist_output)
+        .then(assist_success)
+        .catch(assist_fail);
+    document.getElementById("asapply").value = "Connected";
+    document.getElementById("asdiscon").disabled = false;
+    result.innerHTML = "";
+    output.innerHTML = "";
+}
+
+function assist_run() {
+    if (aconsole.checked == true) {
+        assist_run_console();
+    } else {
+        assist_run_gui();
+    }
 }
 
 function assist_success() {
